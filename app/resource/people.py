8865b472805bdf0ba24f104931c1726c843a38c0
Atmos Maciel <atmos.mps@gmail.com>
2019-04-27 00:13:31 -0300

    Adicionando resource de pessoa.

--- app/resource/people.py
+++ app/resource/people.py
@@ -4,5 +4,21 @@ bp = Blueprint('people', __name__, url_prefix='/people')
 
 
 @bp.route("/", methods=["GET"])
-def get():
+@bp.route("/<int:id>", methods=["GET"])
+def get(id:int =None):
     return "Return All Peoples"
+
+
+@bp.route("/", methods=["POST", ])
+def post():
+    return "Add People"
+
+
+@bp.route("/<int:id>", methods=["PUT"])
+def put(id:int=None):
+    return "Update People"
+
+
+@bp.route("/<int:id>", methods=["DELETE"])
+def delete(id:int=None):
+    return "Delete People"
