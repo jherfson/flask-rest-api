e0b22ac0cce63a63e70f9725cc78b9c43fcd07e6
Atmos Maciel <atmos.mps@gmail.com>
2019-04-27 09:22:19 -0300

    Adicionando funcao de get e post

--- app/resource/people.py
+++ app/resource/people.py
@@ -1,17 +1,36 @@
-from flask import (Flask, make_response, Blueprint)
+from flask import (Flask, make_response, Blueprint, request)
+# from repository import people as people
+from controller import people as people
+import simplejson
 
 bp = Blueprint('people', __name__, url_prefix='/people')
 
 
 @bp.route("/", methods=["GET"])
-@bp.route("/<int:id>", methods=["GET"])
-def get(id:int =None):
-    return "Return All Peoples"
+@bp.route("/<string:fname>", methods=["GET"])
+def get(fname: str = None):
+
+    if fname is None:
+        data = people.read_all()
+    else:
+        data = people.read_one(fname)
+
+    response = make_response(simplejson.dumps(data, ensure_ascii=False), 200)
+    response.headers['Content-Type'] = 'application/json'
+    return response
 
 
 @bp.route("/", methods=["POST", ])
 def post():
-    return "Add People"
+    person = {
+        "fname": request.form['fname'],
+        "lname": request.form['lname']
+    }
+
+    data = people.create(person)
+    response = make_response(simplejson.dumps(data, ensure_ascii=False), 200)
+    response.headers['Content-Type'] = 'application/json'
+    return response
 
 
 @bp.route("/<int:id>", methods=["PUT"])
