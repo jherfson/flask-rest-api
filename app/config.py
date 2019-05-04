5a2a584816b3ac014314fed06dc6ef7e0ba2a29b
Atmos Maciel <atmos.mps@gmail.com>
2019-05-04 16:17:21 -0300

    Adicionando Configurações iniciais da aplicação.

--- app/config.py
+++ app/config.py
@@ -1,4 +1,23 @@
 import os
-import urllib
-from enum import Enum
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    DEBUG = True
+    TESTING = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_RECORD_QUERIES = True
+    FLASK_SLOW_DB_QUERY_TIME = 0.2
+    JSON_AS_ASCII = False
+
+
+class DevelopmentConfig(Config):
+    params_conn = os.path.join(basedir, 'people.db')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + params_conn
+    DEBUG = True
+
+
+class TestConfig(Config):
+    DEBUG = True
+    TESTING = True
