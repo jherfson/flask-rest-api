153ba8e7aa21eef8352c6399845822a874345ff5
Atmos Maciel <atmos.mps@gmail.com>
2019-05-04 16:17:52 -0300

    Criando instancia do Marshmallow.

--- app/model/__init__.py
+++ app/model/__init__.py
@@ -1,3 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 
+# Create the SQLAlchemy db instance
 db = SQLAlchemy()
+
+# Initialize Marshmallow
+ma = Marshmallow()
