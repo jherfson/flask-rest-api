84e1891b41906a9d27da24dfa186cd5f46a8b33c
Atmos Maciel <atmos.mps@gmail.com>
2019-05-05 14:12:32 -0300

    Adicionando primeira versao de read_one no controller de Notas.

--- app/controller/note.py
+++ app/controller/note.py
@@ -17,3 +17,33 @@ def read_all():
     note_schema = NoteSchema(many=True, exclude=["person.notes"])
     data = note_schema.dump(notes).data
     return data
+
+
+def read_one(person_id, note_id):
+    """
+    This function responds to a request for
+    /api/people/{person_id}/notes/{note_id}
+    with one matching note for the associated person
+    :param person_id:       Id of person the note is related to
+    :param note_id:         Id of the note
+    :return:                json string of note contents
+    """
+    # Query the database for the note
+    note = (
+        Note.query.join(Person, Person.person_id == Note.person_id)
+        .filter(Person.person_id == person_id)
+        .filter(Note.note_id == note_id)
+        .one_or_none()
+    )
+
+    # Was a note found?
+    if note is not None:
+        note_schema = NoteSchema()
+        data = note_schema.dump(note).data
+        return data
+
+    # Otherwise, nope, didn't find that note
+    else:
+        abort(404, f"Note not found for Id: {note_id}")
+
+
