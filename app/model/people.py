5967196fa6513fb9c75c9fbe5226a65efe106f04
Atmos Maciel <atmos.mps@gmail.com>
2019-05-05 13:11:39 -0300

    Atualizando Model de Person com Schema de relacionamento com Notas.

--- app/model/people.py
+++ app/model/people.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from . import (db, ma)
+from marshmallow import fields
 
 
 class Person(db.Model):
@@ -11,15 +12,34 @@ class Person(db.Model):
         db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow
     )
     notes = db.relationship(
-        'Note',
+        "Note",
         backref="person",
         cascade="all, delete, delete-orphan",
         single_parent=True,
-        order_by="desc(Note.timestamp)"
+        order_by="desc(Note.timestamp)",
     )
 
 
 class PersonSchema(ma.ModelSchema):
+    def __init__(self, **kwargs):
+        super().__init__(strict=True, **kwargs)
+
     class Meta:
         model = Person
         sqla_session = db.session
+
+    notes = fields.Nested("PersonNoteSchema", default=[], many=True)
+
+
+class PersonNoteSchema(ma.ModelSchema):
+    """
+    This class exists to get around a recursion issue
+    """
+
+    def __init__(self, **kwargs):
+        super().__init__(strict=True, **kwargs)
+
+    note_id = fields.Int()
+    person_id = fields.Int()
+    content = fields.Str()
+    timestamp = fields.Str()
