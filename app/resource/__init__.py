6860f01d66b446f25dd17a99ca96fadfa58e40e7
Atmos Maciel <atmos.mps@gmail.com>
2019-05-07 17:24:39 -0300

    Adicionando messages de retorno no __init__ de resources.

--- app/resource/__init__.py
+++ app/resource/__init__.py
@@ -8,3 +8,8 @@ class ReturnMessage(Enum):
     PUT_SUCCESS_FULL = "{lname} successfully updated."
     DELETED_SUCCESS_FULL = "{person_id} successfully deleted."
     NOT_FOUND_MESSAGE = "The Person with ID {person_id} not found."
+
+    PERSON_NOTE_PARAMETER = "Verifique se o parâmetro person_id {person_id} e note_id {note_id}, \
+    são parâmetro inteiros e válidos."
+
+    GET_NOTE_MESSAGE = "Note not found for Id: {note_id}."
